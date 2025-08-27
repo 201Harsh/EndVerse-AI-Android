5de59a6b250993e3a10eb08a32b6a9de38528288
Harsh <gamerpandeyharsh@gmail.com>
2025-08-27 09:38:05 +0530

    Dashboard UI

---
+++ EndVerse-AI/app/dashboard.tsx
@@ -0,0 +1,22 @@
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "expo-router";
+
+const dashboard = () => {
+  const router = useRouter();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true); // mark as mounted
+  }, []);
+
+  useEffect(() => {
+    if (mounted) {
+      router.replace("/(tabs)/ChatDashboard");
+    }
+  }, [mounted]);
+
+  return null;
+};
+
+export default dashboard;
--- EndVerse-AI/app/index.tsx
+++ EndVerse-AI/app/index.tsx
@@ -1,24 +1,8 @@
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import Welcome from "./Components/Welcome";
-import ChatDashboard from "./(tabs)/ChatDashboard";
-import { useRouter } from "expo-router";
-import { useEffect, useState } from "react";
 
 export default function Index() {
-  const router = useRouter();
-  const [mounted, setMounted] = useState(false);
 
-  // useEffect(() => {
-  //   setMounted(true); // mark as mounted
-  // }, []);
-
-  // useEffect(() => {
-  //   if (mounted) {
-  //     router.replace("/(tabs)/ChatDashboard");
-  //   }
-  // }, [mounted]);
-
-  // return null;
   return (
     <>
       <View className="flex-1 bg-gray-900">
